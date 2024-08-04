f150399d3214b977705b941de180b22f2d86d576
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-08-04 10:56:41 +0300

    Update parse_tululu.py

--- parse_tululu.py
+++ parse_tululu.py
@@ -94,14 +94,8 @@ def get_arguments():
     return args.start_id, args.end_id
 
 
-def main():
-    logging.basicConfig(level=logging.INFO)
-    logger.setLevel(logging.INFO)
-
-    url = 'https://tululu.org'
-    start_id, end_id = get_arguments()
-
-    for book_id in range(start_id, end_id + 1):
+def download_books(url, books_id):
+    for book_id in books_id:
         try:
             response = requests.get(f'{url}/b{book_id}/')
             response.raise_for_status()
@@ -119,6 +113,85 @@ def main():
             logger.warning('Потеряно соединение с интернетом')
             time.sleep(5)
             continue
+
+def get_books_id(url, genre_id , start_page, end_page = None):
+    if end_page:
+        books_id = []
+        for page in range(start_page, end_page+1):
+            url_genre_page = f'{url}/l{genre_id}/{page}'
+            response = requests.get(url_genre_page)
+            response.raise_for_status()
+            check_for_redirect(response)
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            book_cards_selector = '.bookimage a[href^="/b"]'
+            book_cards = soup.select(book_cards_selector)
+            for book_card in book_cards:
+                link = book_card.get('href')
+
+                _, not_sanitized_book_id = link.split('b')
+                book_id = sanitize_filename(not_sanitized_book_id.strip())
+                books_id.append(book_id)
+        return books_id
+
+
+    else:
+        books_id = []
+        while True:
+            try:
+                url_genre_page = f'{url}/l{genre_id}/{start_page}'
+                response = requests.get(url_genre_page)
+                response.raise_for_status()
+                check_for_redirect(response)
+                page_url = response.url
+                print(page_url)
+
+                soup = BeautifulSoup(response.text, 'lxml')
+
+                book_cards_selector = '.bookimage a[href^="/b"]'
+                book_cards = soup.select(book_cards_selector)
+
+                for book_card in book_cards:
+                    link = book_card.get('href')
+
+                    _, not_sanitized_book_id = link.split('b')
+                    book_id = sanitize_filename(not_sanitized_book_id.strip())
+                    books_id.append(book_id)
+
+                start_page += 1
+            except HTTPError:
+                logger.warning(f'Страница {url_genre_page} не существует')
+                return books_id
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    logger.setLevel(logging.INFO)
+
+    url = 'https://tululu.org'
+    genre_id = 55
+    start_page = 700
+
+
+
+
+    books_id = get_books_id(url, genre_id , start_page)
+    print(books_id)
+
+    download_books(url, books_id)
+
+
+
+
 
 
 if __name__ == '__main__':
